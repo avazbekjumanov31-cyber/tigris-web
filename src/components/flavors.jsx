0b09ac54c338290fb7effd10d2f7860b3eb95439
AnorboyevPro <nubekanorboyev012@gmail.com>
2026-09-11 17:31:54 +0500

    new\

--- src/components/flavors.jsx
+++ src/components/flavors.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const Flavors = () => {
   return (
-    <div>Flavors Nurbek</div>
+    <>
+    <div className='bg-green-950 w-full h-180 mt-0 top-0 p-10 '>
+      <h1 className='text-8xl text-white mt-70 ml-150 top-0 uppercase'>FLAVERS</h1>
+      <div className='flex gap-9 mt-10 '>
+        <div className='ml-4'>
+          <img className='w-23 ml-7' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66bb498a7a42435b1c648113_66af7cf488a99d56b5f1d4f2_66ae84eccc4a161325760c35_63bc3e87762ac86e1dc74205_3.png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        <div>
+          <img className='w-23 ml-5' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66bb498bb4e2cdfd7763c817_66ae85b8f73fda6c59c5cb2a_63bc3e6332803f38e330409e_7%2520(1).png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        <div>
+          <img className='w-23 ml-6' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66bb49972bf5e1b9fcaf819f_66ae859de76f47280745ac2a_63bc3e17b8a5d617dff3de79_2%2520(1).png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        <div>
+          <img className='w-23 ml-5' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66bb4999494ed342b9b60979_66ae8567b46d8c496f7b2a1d_63bc3e79762ac8efc9c74173_8.png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        <div>
+          <img className='w-23 ml-5' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66bb49a106293ddf5e1d8601_66ae850f64c49024105ba74f_63bc3e5928439a5b164cf30e_5.png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        <div>
+          <img className='w-23 ml-5' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66bb49ab71278e3d5d9c4212_66ae84db82c2241553045dd6_63bc3e3b56d25f0aabd038ee_6.png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        <div>
+          <img className='w-23 ml-5' src="https://cdn.prod.website-files.com/66ae838a004ef09aef08a56d/66ae84a4606f93d70e3ebf7c_63bc3e0c9a1a870880fdf4a7_1.png" alt="rasm" />
+          <h1 className='text-2xl text-white scale-x-75 origin-left font-bold'>GRAPES SLASH</h1>
+        </div>
+        
+      </div>
+    </div>
+    </>
   )
 }
 
